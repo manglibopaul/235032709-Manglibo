44f95069e74ec6544a6c64170b06e3c83cad4d2e
unknown <manglibopaul@gmail.com>
2025-10-08 20:15:46 +0800

    pushing phpinfo

---
+++ phpinfo.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP INFO</title>
+</head>
+<body>
+    
+    <ul>
+        <li>PHP INFO</li>
+    </ul>
+    
+    <?php
+
+        phpinfo();
+    ?>
+
+</body>
+</html>
